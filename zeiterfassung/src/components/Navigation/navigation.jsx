d90588ddec8ced76779bc845f2fddba5df3c11f1
MV7 <marvin.stoeckemann@web.de>
2025-01-10 11:20:48 +0100

    modified:   zeiterfassung/src/components/Content/content.css     modified:   zeiterfassung/src/components/Navigation/navigation.css 	modified:   zeiterfassung/src/components/Navigation/navigation.jsx

--- zeiterfassung/src/components/Navigation/navigation.jsx
+++ zeiterfassung/src/components/Navigation/navigation.jsx
@@ -1,15 +1,64 @@
+import { useState } from "react";
 import "./navigation.css";
 
 function Navigation() {
+  const [activeLink, setActiveLink] = useState("Startseite");
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <nav className="nav">
-      <a href="#"><i className="ri-home-line"></i> Startseite</a>
-      <a href="#"><i className="ri-user-line"></i> Mitarbeiter</a>
-      <a href="#"><i className="ri-time-line"></i> Zeit</a>
-      <a href="#"><i className="ri-layout-grid-line"></i> Anwesenheitsdisplay</a>
-      <a href="#"><i className="ri-calendar-line"></i> Kalender</a>
-      <a href="#"><i className="ri-lightbulb-line"></i> Ampelkonto</a>
-      <a href="#"><i className="ri-file-list-line"></i> Zeiterfassung</a>
+      <a
+        href="#"
+        className={activeLink === "Startseite" ? "active" : ""}
+        onClick={() => handleLinkClick("Startseite")}
+      >
+        <i className="ri-home-line"></i> <span>Startseite</span>
+      </a>
+      <a
+        href="#"
+        className={activeLink === "Mitarbeiter" ? "active" : ""}
+        onClick={() => handleLinkClick("Mitarbeiter")}
+      >
+        <i className="ri-user-line"></i> <span>Mitarbeiter</span>
+      </a>
+      <a
+        href="#"
+        className={activeLink === "Zeit" ? "active" : ""}
+        onClick={() => handleLinkClick("Zeit")}
+      >
+        <i className="ri-time-line"></i> <span>Zeit</span>
+      </a>
+      <a
+        href="#"
+        className={activeLink === "Anwesenheitsdisplay" ? "active" : ""}
+        onClick={() => handleLinkClick("Anwesenheitsdisplay")}
+      >
+        <i className="ri-layout-grid-line"></i> <span>Anwesenheitsdisplay</span>
+      </a>
+      <a
+        href="#"
+        className={activeLink === "Kalender" ? "active" : ""}
+        onClick={() => handleLinkClick("Kalender")}
+      >
+        <i className="ri-calendar-line"></i> <span>Kalender</span>
+      </a>
+      <a
+        href="#"
+        className={activeLink === "Ampelkonto" ? "active" : ""}
+        onClick={() => handleLinkClick("Ampelkonto")}
+      >
+        <i className="ri-lightbulb-line"></i> <span>Ampelkonto</span>
+      </a>
+      <a
+        href="#"
+        className={activeLink === "Zeiterfassung" ? "active" : ""}
+        onClick={() => handleLinkClick("Zeiterfassung")}
+      >
+        <i className="ri-file-list-line"></i> <span>Zeiterfassung</span>
+      </a>
     </nav>
   );
 }
