80191bb1a67ead5fba3cfb4b989e7e42db8b3b44
MV7 <marvin.stoeckemann@web.de>
2025-01-10 08:53:44 +0100

    modified:   zeiterfassung/src/components/App/App.jsx 	modified:   zeiterfassung/src/components/Content/content.css

--- zeiterfassung/src/components/App/App.jsx
+++ zeiterfassung/src/components/App/App.jsx
@@ -1,31 +1,30 @@
 import Header from "../Header/header";
 import Navigation from "../Navigation/navigation";
-import TerminalCard from "../Content/TerminalCard";
-import TimeAccountsCard from "../Content/TimeAccountsCard";
-import CalendarCard from "../Content/CalendarCard";  
 import ProfileCard from "../Content/ProfileCard";
 import TasksCard from "../Content/TasksCard";
 import VacationCard from "../Content/VacationCard";
+import TimeAccountsCard from "../Content/TimeAccountsCard";
+import TerminalCard from "../Content/TerminalCard";
+import CalendarCard from "../Content/CalendarCard";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Navigation />
-      <main>
-        <ProfileCard
-          name="Max Mustermann"
-          email="max.mustermann@example.com"
-          department="IT-Abteilung"
-          phone="+49 123 456789"
-        />
+    <div className="container">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="navigation">
+        <Navigation />
+      </div>
+      <div className="dashboard-grid">
+        <ProfileCard />
         <TasksCard />
         <VacationCard />
-        <TerminalCard />
         <TimeAccountsCard />
+        <TerminalCard />
         <CalendarCard />
-      </main>
+      </div>
     </div>
   );
 }
