7d1689cd6b409b7816fb395f0c545c352f8717e2
MV7 <marvin.stoeckemann@web.de>
2025-01-14 09:43:53 +0100

    modified:   zeiterfassung/src/components/Content/CalendarCard.jsx

--- zeiterfassung/src/components/Content/CalendarCard.jsx
+++ zeiterfassung/src/components/Content/CalendarCard.jsx
@@ -3,7 +3,6 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./Content.css";
 
-
 export default function CalendarCard() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const today = new Date().toLocaleDateString("de-DE", {
@@ -13,15 +12,19 @@ export default function CalendarCard() {
     day: "numeric",
   });
 
+  const handleOpenCalendar = () => {
+    alert("Mein Kalender wird geöffnet.");
+  };
+
   return (
     <div className="card-calendar-card">
       <h2>Heute ist der {today}</h2>
       <div className="calendar">
         <Calendar value={selectedDate} onChange={setSelectedDate} />
       </div>
-      <button>
+      <button onClick={handleOpenCalendar}>
         <i className="ri-navigation-line"></i> Meinen Kalender öffnen
-      </button>
+      </button> 
     </div>
   );
 }
